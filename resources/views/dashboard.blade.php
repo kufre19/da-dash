1459600fcfc4034292a763b217d738f13d468a4d
Emmanuel Kufre <whitemaxwell5@gmial.com>
2023-02-16 09:16:09 +0100

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.custom.app')
+
+@section('page_content')
+    
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800"> Admin Dashboard</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+
+          
+@endsection
+          
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::group(['middleware'=>"auth",'prefix'=>"dashboard"], function(){
+
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get("/account/create",[AdminController::class,"createUserAccount_page"]);
+    Route::post("/account/create",[AdminController::class,"createUserAccount"]);
+    Route::get("/account/list",[AdminController::class,"listAccounts"]);
+    Route::get("/account/edit/{id}",[AdminController::class,"editAccount_page"]);
+    Route::post("/account/edit/",[AdminController::class,"editUserAccount"]);
+    Route::post("/account/delete/{id}",[AdminController::class,"deleAccount"]);
+
+
+
+});
+
+require __DIR__.'/auth.php';
